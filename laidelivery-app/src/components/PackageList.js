dfbfac289b4455b60903c90a57a018d5271f3ff9
renee <renee.xu.j@gmail.com>
2021-12-19 11:07:45 -0600

    update login

--- laidelivery-app/src/components/PackageList.js
+++ laidelivery-app/src/components/PackageList.js
@@ -6,114 +6,114 @@ import { PlusOutlined } from "@ant-design/icons";
 const { Option } = Select;
 
 const AddToCartButton = ({ itemId }) => {
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const AddToCart = () => {
-    setLoading(true);
-    addItemToCart(itemId)
-        .then(() => message.success(`Successfully add item`))
-        .catch((err) => message.error(err.message))
-        .finally(() => {
-          setLoading(false);
-        });
-  };
+    const AddToCart = () => {
+        setLoading(true);
+        addItemToCart(itemId)
+            .then(() => message.success(`Successfully add item`))
+            .catch((err) => message.error(err.message))
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
-  return (
-      <Tooltip title="Add to shopping cart">
-        <Button
-            loading={loading}
-            type="primary"
-            icon={<PlusOutlined />}
-            onClick={AddToCart}
-        />
-      </Tooltip>
-  );
+    return (
+        <Tooltip title="Add to shopping cart">
+            <Button
+                loading={loading}
+                type="primary"
+                icon={<PlusOutlined />}
+                onClick={AddToCart}
+            />
+        </Tooltip>
+    );
 };
 
 const PackageList = () => {
-  const [packageData, setPackageData] = useState([]);
-  const [curPackage, setCurpackage ] = useState();
-  const [orders, setOrders] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [loadingPack, setLoadingPack] = useState(false);
+    const [packageId, setPackageId] = useState([]);
+    const [curPackage, setCurpackage ] = useState();
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [loadingPack, setLoadingPack] = useState(false);
 
-  useEffect(() => {
-    setLoadingPack(true);
-    getOrder()
-        .then((data) => {
-          setOrders(data);
-        })
-        .catch((err) => {
-          message.error(err.message);
-        })
-        .finally(() => {
-          setLoadingPack(false);
-        });
-  }, []);
+    useEffect(() => {
+        setLoadingPack(true);
+        getOrder()
+            .then((data) => {
+                setOrders(data);
+            })
+            .catch((err) => {
+                message.error(err.message);
+            })
+            .finally(() => {
+                setLoadingPack(false);
+            });
+    }, []);
 
-  useEffect(() => {
-    if (curPackage) {
-      setLoading(true);
-      getPackage(curPackage)
-          .then((data) => {
-            setPackageData(data);
-          })
-          .catch((err) => {
-            message.error(err.message);
-          })
-          .finally(() => {
-            setLoading(false);
-          });
-    }
-  }, [curPackage]);
+    useEffect(() => {
+        if (curPackage) {
+            setLoading(true);
+            getPackage(curPackage)
+                .then((data) => {
+                    setPackageId(data);
+                })
+                .catch((err) => {
+                    message.error(err.message);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
+        }
+    }, [curPackage]);
 
-  return (
-      <>
-        <Select
-            value={curPackage}
-            onSelect={(value) => setCurpackage(value)}
-            placeholder="Select a order"
-            loading={loadingPack}
-            style={{ width: 300 }}
-            onChange={() => {}}
-        >
-          {orders.map((item) => {
-            return <Option value={item.id}>{item.name}</Option>;
-          })}
-        </Select>
-        {curPackage && (
-            <List
-                style={{ marginTop: 20 }}
-                loading={loading}
-                grid={{
-                  gutter: 16,
-                  xs: 1,
-                  sm: 2,
-                  md: 4,
-                  lg: 4,
-                  xl: 3,
-                  xxl: 3,
-                }}
-                dataSource={packageData}
-                renderItem={(item) => (
-                    <List.Item>
-                      <Card
-                          title={item.name}
-                          extra={<AddToCartButton itemId={item.id} />}
-                      >
-                        <img
-                            src={item.imageUrl}
-                            alt={item.name}
-                            style={{ height: 340, width: "100%", display: "block" }}
-                        />
-                        {`Price: ${item.price}`}
-                      </Card>
-                    </List.Item>
-                )}
-            />
-        )}
-      </>
-  );
+    return (
+        <>
+            <Select
+                value={curPackage}
+                onSelect={(value) => setCurpackage(value)}
+                placeholder="Select a order"
+                loading={loadingPack}
+                style={{ width: 300 }}
+                onChange={() => {}}
+            >
+                {orders.map((item) => {
+                    return <Option value={item.id}>{item.name}</Option>;
+                })}
+            </Select>
+            {curPackage && (
+                <List
+                    style={{ marginTop: 20 }}
+                    loading={loading}
+                    grid={{
+                        gutter: 16,
+                        xs: 1,
+                        sm: 2,
+                        md: 4,
+                        lg: 4,
+                        xl: 3,
+                        xxl: 3,
+                    }}
+                    dataSource={packageId}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Card
+                                title={item.name}
+                                extra={<AddToCartButton itemId={item.id} />}
+                            >
+                                <img
+                                    src={item.imageUrl}
+                                    alt={item.name}
+                                    style={{ height: 340, width: "100%", display: "block" }}
+                                />
+                                {`Price: ${item.price}`}
+                            </Card>
+                        </List.Item>
+                    )}
+                />
+            )}
+        </>
+    );
 };
 
 export default PackageList;
--- laidelivery-app/src/components/SignupForm.js
+++ laidelivery-app/src/components/SignupForm.js
@@ -4,103 +4,103 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { signup } from "../utils";
 
 class SignupForm extends React.Component {
-  state = {
-    displayModal: false,
-  };
+    state = {
+        displayModal: false,
+    };
 
-  handleCancel = () => {
-    this.setState({
-      displayModal: false,
-    });
-  };
-
-  signupOnClick = () => {
-    this.setState({
-      displayModal: true,
-    });
-  };
-
-  onFinish = (data) => {
-    signup(data)
-        .then(() => {
-          this.setState({
+    handleCancel = () => {
+        this.setState({
             displayModal: false,
-          });
-          message.success(`Successfully signed up`);
-        })
-        .catch((err) => {
-          message.error(err.message);
         });
-  };
+    };
 
-  render = () => {
-    return (
-        <>
-          <Button shape="round" type="primary" onClick={this.signupOnClick}>
-            Register
-          </Button>
-          <Modal
-              title="Register"
-              visible={this.state.displayModal}
-              onCancel={this.handleCancel}
-              footer={null}
-              destroyOnClose={true}
-          >
-            <Form
-                name="normal_register"
-                initialValues={{ remember: true }}
-                onFinish={this.onFinish}
-                preserve={false}
-            >
-              <Form.Item
-                  name="email"
-                  rules={[{ required: true, message: "Please input your email!" }]}
-              >
-                <Input prefix={<UserOutlined />} placeholder="Email" />
-              </Form.Item>
-              <Form.Item
-                  name="Username"
-                  rules={[
-                    { required: true, message: "Please input Username!" },
-                  ]}
-              >
-                <Input placeholder="Username" />
-              </Form.Item>
-              <Form.Item
-                  name="password"
-                  rules={[
-                    { required: true, message: "Please input your password!" },
-                  ]}
-              >
-                <Input prefix={<LockOutlined />} placeholder="Password" />
-              </Form.Item>
-              <Form.Item
-                  name="firstName"
-                  rules={[
-                    { required: true, message: "Please input your first name!" },
-                  ]}
-              >
-                <Input placeholder="firstname" />
-              </Form.Item>
-              <Form.Item
-                  name="lastName"
-                  rules={[
-                    { required: true, message: "Please input your last name!" },
-                  ]}
-              >
-                <Input placeholder="lastname" />
-              </Form.Item>
+    signupOnClick = () => {
+        this.setState({
+            displayModal: true,
+        });
+    };
 
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Register
+    onFinish = (data) => {
+        signup(data)
+            .then(() => {
+                this.setState({
+                    displayModal: false,
+                });
+                message.success(`Successfully signed up`);
+            })
+            .catch((err) => {
+                message.error(err.message);
+            });
+    };
+
+    render = () => {
+        return (
+            <>
+                <Button shape="round" type="primary" onClick={this.signupOnClick}>
+                    Register
                 </Button>
-              </Form.Item>
-            </Form>
-          </Modal>
-        </>
-    );
-  };
+                <Modal
+                    title="Register"
+                    visible={this.state.displayModal}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                    destroyOnClose={true}
+                >
+                    <Form
+                        name="normal_register"
+                        initialValues={{ remember: true }}
+                        onFinish={this.onFinish}
+                        preserve={false}
+                    >
+                        <Form.Item
+                            name="email"
+                            rules={[{ required: true, message: "Please input your email!" }]}
+                        >
+                            <Input prefix={<UserOutlined />} placeholder="Email" />
+                        </Form.Item>
+                        <Form.Item
+                            name="Username"
+                            rules={[
+                                { required: true, message: "Please input Username!" },
+                            ]}
+                        >
+                            <Input placeholder="Username" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                { required: true, message: "Please input your password!" },
+                            ]}
+                        >
+                            <Input prefix={<LockOutlined />} placeholder="Password" />
+                        </Form.Item>
+                        <Form.Item
+                            name="firstName"
+                            rules={[
+                                { required: true, message: "Please input your first name!" },
+                            ]}
+                        >
+                            <Input placeholder="firstname" />
+                        </Form.Item>
+                        <Form.Item
+                            name="lastName"
+                            rules={[
+                                { required: true, message: "Please input your last name!" },
+                            ]}
+                        >
+                            <Input placeholder="lastname" />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Register
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Modal>
+            </>
+        );
+    };
 }
 
 export default SignupForm;
