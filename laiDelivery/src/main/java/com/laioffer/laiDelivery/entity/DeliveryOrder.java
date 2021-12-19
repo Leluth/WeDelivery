0aefac83472dd8f33433465e9654f4a42227d031
Hung-Hsi Lin <hung-hsilin@hung-hsis-air.lan>
2021-12-19 14:59:55 -0800

    signin completed

---
+++ laiDelivery/src/main/java/com/laioffer/laiDelivery/entity/DeliveryOrder.java
@@ -0,0 +1,145 @@
+package com.laioffer.laiDelivery.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "deliverorder")
+public class DeliveryOrder implements Serializable {
+
+    private static final long serialVersionUID = -2455760938054036364L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private double weight;
+
+    private String packageType;
+
+    private String packageId;
+
+    private String shippingFrom;
+
+    private String shippingTo;
+
+    private String deliveryType;
+
+    private String serviceType;
+
+    private double deliveryTime;
+
+    private double pickUpTime;
+
+    private String centerId;
+
+    private double price;
+
+    private String orderId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getPackageType() {
+        return packageType;
+    }
+
+    public void setPackageType(String packageType) {
+        this.packageType = packageType;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public void setPackageId(String packageId) {
+        this.packageId = packageId;
+    }
+
+    public String getShippingFrom() {
+        return shippingFrom;
+    }
+
+    public void setShippingFrom(String shippingFrom) {
+        this.shippingFrom = shippingFrom;
+    }
+
+    public String getShippingTo() {
+        return shippingTo;
+    }
+
+    public void setShippingTo(String shippingTo) {
+        this.shippingTo = shippingTo;
+    }
+
+    public String getDeliveryType() {
+        return deliveryType;
+    }
+
+    public void setDeliveryType(String deliveryType) {
+        this.deliveryType = deliveryType;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public double getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(double deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public double getPickUpTime() {
+        return pickUpTime;
+    }
+
+    public void setPickUpTime(double pickUpTime) {
+        this.pickUpTime = pickUpTime;
+    }
+
+    public String getCenterId() {
+        return centerId;
+    }
+
+    public void setCenterId(String centerId) {
+        this.centerId = centerId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+}
