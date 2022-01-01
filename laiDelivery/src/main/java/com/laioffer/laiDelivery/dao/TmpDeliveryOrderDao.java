75d530b7674ae423f274f1f0b322ad43c238a2b4
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-12-31 18:08:48 -0800

    [Add] Add update tmpDB backend

--- laiDelivery/src/main/java/com/laioffer/laiDelivery/dao/TmpDeliveryOrderDao.java
+++ laiDelivery/src/main/java/com/laioffer/laiDelivery/dao/TmpDeliveryOrderDao.java
@@ -5,13 +5,10 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.query.Query;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import com.laioffer.laiDelivery.dao.DeliveryOrderDao;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
