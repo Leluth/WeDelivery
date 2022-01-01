ed7ac01e30f0247c17b15233304db9cc612037be
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-12-31 22:55:33 -0800

    [Add] Add order status

--- laidelivery-app/src/components/MyOrders.js
+++ laidelivery-app/src/components/MyOrders.js
@@ -1,15 +1,19 @@
 import React, {Component} from "react";
-import {Col, List, message, Row} from "antd";
+import {Col, List, message, Row, Steps} from "antd";
 import OrderCard from "./OrderCard";
 import MapContainer from "./MapContainer";
 import {getOrders} from "../utils";
+import {getCurrentTimeStr, smallerThan} from "../timeUtils";
+
+const { Step } = Steps;
 
 class MyOrders extends Component {
     constructor(props) {
         super(props);
         this.state = {
             geoInfo: {},
-            data: []
+            data: [],
+            status: -1
         };
     }
 
@@ -25,6 +29,19 @@ class MyOrders extends Component {
             })
     }
 
+    updateStatus(item) {
+        if (smallerThan(getCurrentTimeStr(), item.pickUpTime)) {
+            // console.log('ordered')
+            this.setState({status: 0})
+        } else if (smallerThan(getCurrentTimeStr(), item.deliveryTime)) {
+            // console.log('picked up')
+            this.setState({status: 1})
+        } else {
+            // console.log('delivered')
+            this.setState({status: 2})
+        }
+    }
+
     render() {
         return (
             <Row className='main'>
@@ -36,12 +53,24 @@ class MyOrders extends Component {
                             <List.Item>
                                 <OrderCard info={item} onChange={(e) => {
                                     this.setState({geoInfo: item})
+                                    this.updateStatus(item)
                                 }}/>
                             </List.Item>
                         )}
                     />
                 </Col>
                 <Col span={17} className="right-side">
+                    {
+                        this.state.status === -1? (
+                            <></>
+                        ) : (
+                            <Steps progressDot current={this.state.status}>
+                                <Step title="Ordered" description="Our robot/drone is on the way" />
+                                <Step title="Picked Up" description="Almost there!" />
+                                <Step title="Delivered" description="Your order has been delivered" />
+                            </Steps>
+                        )
+                    }
                     <MapContainer geoInfo={this.state.geoInfo}/>
                 </Col>
             </Row>
